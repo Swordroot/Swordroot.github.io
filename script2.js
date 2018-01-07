5d26a7d23a84457712f8f673ffbf00b2481ad902
Makoto Tone <makotaiko@yahoo.co.jp>
2018-01-08 01:22:15 +0900

    fix bug

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 var today = new Date();
 
-var month = today.getMonth();
-var day = today.getDay();
+var month = today.getMonth()+1;
+var day = today.getDate();
 
 var md = month * 100 + day;
 
@@ -10,7 +10,7 @@ showClasses();
 function showClasses(){
     for(var i = 1;i<=10;i++){
         var filterredArray = classesData.filter(function(element){
-            return element.学科番号 == 1 && element.月日 > md;
+            return element.学科番号 == i && element.月日 > md;
         }).sort(function(a,b){
             if(a.月日 > b.月日){
                 return 1;
